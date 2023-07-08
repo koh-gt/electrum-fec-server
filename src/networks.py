40ef25f4889da8575bef0a5d836845667784e0c3
koh-gt <101822992+koh-gt@users.noreply.github.com>
2023-07-09 04:12:08 +0800

    Update networks.py

--- src/networks.py
+++ src/networks.py
@@ -10,5 +10,15 @@ params = {
         'pubkey_address': 111,
         'script_address': 196,
         'genesis_hash': '000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943'
+    },
+    'ferrite_main': {
+        'pubkey_address': 36,
+        'script_address': 5,
+        'genesis_hash': '46ca17415c18e43f5292034ebf9bbd10de80a61fc6dc17180e6609f33d3b48f3'
+    },
+    'ferrite_test': {
+        'pubkey_address': 111,
+        'script_address': 196,
+        'genesis_hash': '7a9f43d6e86eefa66e2b79918b2235c9362106f3d9f11f37f7a33450ceae73c1'
     }
 }
