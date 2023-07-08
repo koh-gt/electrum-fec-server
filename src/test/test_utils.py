382e45da891181a15ef8a693145fad3565c36aa8
koh-gt <101822992+koh-gt@users.noreply.github.com>
2023-07-09 04:36:50 +0800

    Update test_utils.py

--- src/test/test_utils.py
+++ src/test/test_utils.py
@@ -11,14 +11,14 @@ class UtilTest(unittest.TestCase):
         self.assertEqual(hash_160_to_address('04e9fca1'.decode('hex')), None)
         self.assertEqual(hash_160_to_address('04e9fca1f96e021dfaf35bbea267ec2c60787c1b1337'.decode('hex')), None)
         self.assertEqual(hash_160_to_address('1ad3b0b711f211655a01142fbb8fecabe8e30b93'.decode('hex')),
-                         '13SrAVFPVW1txSj34B8Bd6hnDbyPsVGa92')
+                         'FXcZcPym3zhSQ3k9wH7f5cW6smFMVaMUQY')
 
 
     def test_bc_address_to_hash_160(self):
         self.assertEqual(bc_address_to_hash_160(None), None)
         self.assertEqual(bc_address_to_hash_160(''), None)
-        self.assertEqual(bc_address_to_hash_160('13SrAVFPVW1txSj34B8Bd6hnDbyPsVGa921337'), None)
-        self.assertEqual(bc_address_to_hash_160('13SrAVFPVW1txSj34B8Bd6hnDbyPsVGa92').encode('hex'),
+        self.assertEqual(bc_address_to_hash_160('FXcZcPym3zhSQ3k9wH7f5cW6smFMVaMUQY'), None)
+        self.assertEqual(bc_address_to_hash_160('FXcZcPym3zhSQ3k9wH7f5cW6smFMVaMUQY').encode('hex'),
                                                 '1ad3b0b711f211655a01142fbb8fecabe8e30b93')
 
 
