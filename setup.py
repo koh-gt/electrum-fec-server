99c3b882d46f57c9db320f478649eea451dd2ff2
koh-gt <101822992+koh-gt@users.noreply.github.com>
2023-07-09 03:28:27 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,30 @@
 from setuptools import setup
 
 setup(
-    name="electrum-server",
+    name="electrum-fec-server",
     version="1.0",
-    scripts=['run_electrum_server.py','electrum-server'],
+    scripts=['run_electrum_fec_server.py','electrum-fec-server'],
     install_requires=['plyvel','jsonrpclib', 'irc >= 11, <=14.0'],
     package_dir={
-        'electrumserver':'src'
+        'electrumfecserver':'src'
         },
     py_modules=[
-        'electrumserver.__init__',
-        'electrumserver.utils',
-        'electrumserver.storage',
-        'electrumserver.deserialize',
-        'electrumserver.networks',
-        'electrumserver.blockchain_processor',
-        'electrumserver.server_processor',
-        'electrumserver.processor',
-        'electrumserver.version',
-        'electrumserver.ircthread',
-        'electrumserver.stratum_tcp'
+        'electrumfecserver.__init__',
+        'electrumfecserver.utils',
+        'electrumfecserver.storage',
+        'electrumfecserver.deserialize',
+        'electrumfecserver.networks',
+        'electrumfecserver.blockchain_processor',
+        'electrumfecserver.server_processor',
+        'electrumfecserver.processor',
+        'electrumfecserver.version',
+        'electrumfecserver.ircthread',
+        'electrumfecserver.stratum_tcp'
     ],
-    description="Bitcoin Electrum Server",
+    description="Ferrite Electrum Server",
     author="Thomas Voegtlin",
     author_email="thomasv@electrum.org",
     license="MIT Licence",
-    url="https://github.com/spesmilo/electrum-server/",
-    long_description="""Server for the Electrum Lightweight Bitcoin Wallet"""
+    url="https://github.com/koh-gt/electrum-fec-server/",
+    long_description="""Server for the Electrum Lightweight Ferrite Wallet"""
 )
